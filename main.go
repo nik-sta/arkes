9a292d88c343c45d8f57a23c1800d49745cb588b
Nikola Stankovic <nikola@stankovic.xyz>
2018-02-23 16:53:39 +0100

    initial commit with rest api

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
-package arkes
+package main
+
+import "encoding/json"
+import "log"
+import "net/http"
+import "github.com/gorilla/mux"
+
+type Greeting struct {
+	Message string `json:"message,omitempty"`
+}
+
+func main()  {
+	router := mux.NewRouter()
+	router.HandleFunc("/hello", SayHello).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func SayHello(w http.ResponseWriter, r *http.Request)  {
+	greeting := Greeting{"Hello :)"}
+	json.NewEncoder(w).Encode(greeting)
+}
